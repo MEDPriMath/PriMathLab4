6123436c7a5f26742908abb0de50b2981be81a05
Danilaa56 <danila.aronov@gmail.com>
2022-05-28 12:36:16 +0300

    feat: calculating eigen vectors

--- src/ru/itmo/checkers/MatrixAlgorithmChecker.java
+++ src/ru/itmo/checkers/MatrixAlgorithmChecker.java
@@ -1,10 +1,11 @@
 package ru.itmo.checkers;
 
 import ru.itmo.markdown.blocks.MarkdownBlock;
+import ru.itmo.primath.generator.SymmetryMatrixGenerator;
+import ru.itmo.primath.matrix.Matrix;
 
 import java.util.List;
 
 public interface MatrixAlgorithmChecker {
-    void check();
-    List<MarkdownBlock> getMarkdownBlocks();
+    <T extends Matrix<T>> List<MarkdownBlock> check(List<SymmetryMatrixGenerator<T>> matrixGenerators, double precision, int matrixSize);
 }
--- src/ru/itmo/primath/algo/JacobiRotation.java
+++ src/ru/itmo/primath/algo/JacobiRotation.java
@@ -3,6 +3,8 @@ package ru.itmo.primath.algo;
 import ru.itmo.primath.matrix.Matrix;
 import ru.itmo.primath.vector.Vector;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import static java.lang.Math.PI;
 import static java.lang.Math.abs;
 import static java.lang.Math.cos;
@@ -17,27 +19,28 @@ public class JacobiRotation {
     private record MatrixJacobiStat(double nonDiagonalSum, int maxRow, int maxColumn) {
     }
 
-    public static <TMatrix extends Matrix<TMatrix>> Vector calculate(TMatrix matrix, double epsilon) {
-        TMatrix a = matrix;
-
+    public static <TMatrix extends Matrix<TMatrix>> JacobiRotationResult<TMatrix> calculate(TMatrix matrix, double epsilon) {
         MatrixJacobiStat stat;
-        while ((stat = analyzeMatrix(a)).nonDiagonalSum > epsilon) {
-            var phi = phiForRotation(a, stat);
-            a = jakobiRotate(a, stat.maxRow, stat.maxColumn, phi);
+
+        AtomicReference<TMatrix> eigenVectors = new AtomicReference<>(matrix.createIdentity(matrix.rows));
+
+        while ((stat = analyzeMatrix(matrix)).nonDiagonalSum > epsilon) {
+            var phi = phiForRotation(matrix, stat);
+            matrix = jakobiRotate(matrix, stat.maxRow, stat.maxColumn, phi, eigenVectors);
         }
 
         Vector eigenValues = new Vector(matrix.rows);
         for (int i = 0; i < matrix.rows; i++) {
-            eigenValues.set(i, a.get(i, i));
+            eigenValues.set(i, matrix.get(i, i));
         }
 
-        return eigenValues;
+        return new JacobiRotationResult(eigenVectors.get(), eigenValues);
     }
 
     /**
      * Проходимся по матрице:
-     *      считаем корень суммы квадратов недиагональных элементов - нужен для оценки состояния матрицы
-     *      находим самый большой по модулю недиагональный элемент - для дальнейшего удаления
+     * считаем корень суммы квадратов недиагональных элементов - нужен для оценки состояния матрицы
+     * находим самый большой по модулю недиагональный элемент - для дальнейшего удаления
      */
     private static MatrixJacobiStat analyzeMatrix(Matrix<?> matrix) {
         double maxValue = 0;
@@ -83,18 +86,22 @@ public class JacobiRotation {
 
     /**
      * Осуществляем вращение матрицы вокруг определённой точки туда и обратно
-     * @param m исходная матрица
-     * @param row строка с наибольшим недиагнональныым элементом
+     *
+     * @param m      исходная матрица
+     * @param row    строка с наибольшим недиагнональныым элементом
      * @param column столбец с наибольшим недиагнональныым элементом
-     * @param phi угол поворота
+     * @param phi    угол поворота
      * @return возвращает новый объект - полученную матрицу, где не будет того самого большого элемента
      */
-    private static <TMatrix extends Matrix<TMatrix>> TMatrix jakobiRotate(TMatrix m, int row, int column, double phi) {
+    private static <TMatrix extends Matrix<TMatrix>> TMatrix jakobiRotate(TMatrix m, int row, int column, double phi,
+                                                                          AtomicReference<TMatrix> eigenVectors) {
         var u = m.createIdentity(m.rows);
         u.set(row, row, cos(phi));
         u.set(row, column, -sin(phi));
         u.set(column, row, sin(phi));
         u.set(column, column, cos(phi));
+
+        eigenVectors.set(eigenVectors.get().multiply(u));
 
         var tmp = m.multiply(u);
 
