14a77f6f76dd53169c6428da3a8538c17fa4ecb8
Danilaa56 <danila.aronov@gmail.com>
2022-05-28 12:36:29 +0300

    feat: fix main checker

--- src/ru/itmo/primath/Main.java
+++ src/ru/itmo/primath/Main.java
@@ -5,27 +5,34 @@ import ru.itmo.markdown.MarkdownDocument;
 import ru.itmo.primath.algo.JacobiRotation;
 import ru.itmo.primath.generator.DiagonallyDominantArrayMatrixGenerator;
 import ru.itmo.primath.generator.SymmetryMatrixGenerator;
+import ru.itmo.primath.matrix.ArrayMatrix;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     /**
-     *  В ЭТОЙ ЛАБОРАТОРНОЙ РАБОТЕ МАТРИЦУ ДОЛЖНЫ БЫТЬ СИММЕТРИЧНЫ
+     * В ЭТОЙ ЛАБОРАТОРНОЙ РАБОТЕ МАТРИЦЫ ДОЛЖНЫ БЫТЬ СИММЕТРИЧНЫ
      */
     public static void main(String[] args) throws Exception {
         var matrix = new DiagonallyDominantArrayMatrixGenerator(5).generate(50);
         JacobiRotation.calculate(matrix, 1E-2);
 
-        List<SymmetryMatrixGenerator> symmetryMatrixGenerators = new ArrayList<>();
-        symmetryMatrixGenerators.add(new DiagonallyDominantArrayMatrixGenerator(1));
+//        List<DiagonallyDominantArrayMatrixGenerator> symmetryMatrixGenerators = new ArrayList<>();
+//        symmetryMatrixGenerators.add(new DiagonallyDominantArrayMatrixGenerator(1));
 
-        JacobiRotationChecker jacobiRotationChecker = new JacobiRotationChecker(symmetryMatrixGenerators, 1E-5, 10);
-        jacobiRotationChecker.check();
+        var generators = new ArrayList<SymmetryMatrixGenerator<ArrayMatrix>>();
+        generators.add(new DiagonallyDominantArrayMatrixGenerator(1));
 
-        MarkdownDocument markdownDocument = new MarkdownDocument(jacobiRotationChecker.getMarkdownBlocks());
+//        JacobiRotationChecker jacobiRotationChecker = new JacobiRotationChecker(generators, 1E-5, 10);
+//        jacobiRotationChecker.check();
+        var jacobiRotationCheckMD = new JacobiRotationChecker().check(
+                generators,
+                1E-5,
+                10
+        );
+
+        MarkdownDocument markdownDocument = new MarkdownDocument(jacobiRotationCheckMD);
         markdownDocument.toMarkdownFile("report.md");
 
 //        MarkdownDocument.toHTML("report.md", "..\\ReportLab4\\index.html");
